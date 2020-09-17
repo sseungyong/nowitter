44fece143f74fcc16ac35026e9599ad091bf0eb2
SEUNGYONG KIM <sseungyong@gmail.com>
2020-09-17 14:55:27 +0900

    add authentication

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,26 @@
-import React from "react";
-import AppRouter from "./Router";
-import Router from "./Router";
+import React, { useState, useEffect } from "react";
+import AppRouter from "components/Router";
+import { authService } from "fbase";
 
 function App() {
-  return <AppRouter />;
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
+  return (
+    <>
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing..."}
+      <footer>&copy; Nowitter {new Date().getFullYear()}</footer>
+    </>
+  );
 }
 
 export default App;
--- src/components/Router.js
+++ src/components/Router.js
@@ -1,23 +1,30 @@
-import React, { useState } from "react";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
-import Auth from "../routes/Auth";
-import Home from "../routes/Home";
+import React from "react";
+import { HashRouter as Router, Redirect, Route, Switch } from "react-router-dom";
+import Auth from "routes/Auth";
+import Home from "routes/Home";
+import Navigation from "components/Navigation";
+import Profile from "routes/Profile";
 
-const AppRouter = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
+      {isLoggedIn && <Navigation />}
       <Switch>
         {isLoggedIn ? (
           <>
             <Route expact path="/">
               <Home></Home>
             </Route>
+            <Route expact path="/profile">
+              <Profile></Profile>
+            </Route>
+            {/* <Redirect from="*" to="/"/> */}
           </>
         ) : (
           <Route exact path="/">
             <Auth></Auth>
           </Route>
+          // <Redirect from="*" to="/"/>
         )}
       </Switch>
     </Router>
