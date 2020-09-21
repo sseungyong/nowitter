7aa1dc723438640a2a716de911771c2f24cb3c08
sseungyong <sseungyong@gmail.com>
2020-09-21 18:18:52 +0900

    Updates

---
+++ precache-manifest.80c2c246104f25408b5f416b8c48a55b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf98d320a213a241e8a0d89bb915a884",
+    "url": "/nowitter/index.html"
+  },
+  {
+    "revision": "0de41fdcf247a99049b8",
+    "url": "/nowitter/static/css/main.fad466ed.chunk.css"
+  },
+  {
+    "revision": "6590b39e580960ea23aa",
+    "url": "/nowitter/static/js/2.eda42b68.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/nowitter/static/js/2.eda42b68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0de41fdcf247a99049b8",
+    "url": "/nowitter/static/js/main.9e161910.chunk.js"
+  },
+  {
+    "revision": "308073fbb495dfc8ec87",
+    "url": "/nowitter/static/js/runtime-main.a4bb60b2.js"
+  }
+]);
